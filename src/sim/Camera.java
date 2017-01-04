271ef311121b117e09fbc0ef7119eaa109167dc9
Blevenson <blevenson68@gmail.com>
2017-01-04 11:58:21 -0800

    Connects simulator camera to AxisCamera.  Joystick can store values to run during teleop

--- src/sim/Camera.java
+++ src/sim/Camera.java
@@ -2,8 +2,24 @@ package sim;
 
 import interfaces.CameraReader;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+/**
+ * Grabs images from the robot camera
+ * http://localhost:7663/
+ * 
+ * @author Brett Levenson
+ *
+ */
 public class Camera implements CameraReader{
 
 	private String id;
@@ -15,12 +31,26 @@ public class Camera implements CameraReader{
 	@Override
 	public Mat getImage() {
 		if(id.equals("axisCamera")){
+			try {
+				URL url = new URL("http://localhost:7663/");
+				BufferedImage image = ImageIO.read(url);
+				return bufferedImageToMat(image);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			//return images of goal
 		}
 		else{
 			//images of front intake - or whatever the usb camera sees
 		}
 		return null;
+	}
+	
+	private Mat bufferedImageToMat(BufferedImage bi) {
+		  Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+		  byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+		  mat.put(0, 0, data);
+		  return mat;
 	}
 
 }
--- src/sim/Joystick.java
+++ src/sim/Joystick.java
@@ -5,6 +5,10 @@ import interfaces.JoystickReader;
 public class Joystick implements JoystickReader{
 
 	private int port;
+	private final double[] values = new double[]{0, -1, 1};
+	
+	int index = -1;
+	int calls = 0;
 	
 	public Joystick(int port){
 		this.port = port;
@@ -13,6 +17,16 @@ public class Joystick implements JoystickReader{
 	@Override
 	public double getRawAxis(int axis) {
 		return 0;
+		
+//		if(index >= values.length)
+//			return 0;
+//		
+//		if(calls % 100 == 0)
+//			index++;
+//		calls++;
+//		
+//		System.out.println("Index: " + index + "\tValue: " + values[index]);
+//		return values[index];
 	}
 
 	@Override
